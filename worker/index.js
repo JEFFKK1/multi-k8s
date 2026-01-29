eb9a1e342f091c0db67c6f88998f0e049dacc9cb
jeffkk1 <jkaufman512@gmail.com>
2026-01-29 10:43:05 -0500

    some server, worker changes

--- worker/index.js
+++ worker/index.js
@@ -1,70 +1,22 @@
 import redis from 'redis'
 
 import { redisHost, redisPort} from './keys.js';
-// const client = redis.createClient(
-//     redisHost,
-//     redisPort,
-//   //  retry_strategy= () => 10
-// )
+
 const client = redis.createClient({
   host: redisHost,
   port: redisPort,
   retry_strategy: () => 1000,
 });
 
-
-//const client = redis.createClient({ url: 'redis://localhost:6379' });
-
 const sub = client.duplicate()
 console.log('sub is connecting' )
 sub.on('error', err => {
    console.log('reids error',err)
  });
-//
-//sub.connect();
-//console.log('redisSub for worker = :', redisClient )
- 
-
-async function doSubscribe() {
-        console.log('going to connect now')
-        console.log('are we turning on message')
-        sub.on('error', err => {
-            console.log('reids error',err)
-        });
-        sub.on('message', (channel, message) => {
-            console.log(`Received the following message from ${channel}: ${message}`);
-                // redisClient.hset('values', message, fib(parseInt(message)))
-        })
-             //await redisClient.subscribe('insert')
-        console.log('are we turning on subscribe')
-        sub.subscribe('insert', (message) => {
-            console.log('the message = ', message)
-             console.log('fib solution: ',fib(parseInt(message)))
-          //  finish(message)
-        })   
-    }
-async function doPublish() {
-    const pub = redis.createClient({
-        host: redisHost,
-        port: redisPort,
-        retry_strategy: () => 1000,
-    });;
-   // await pub.connect();
-    // await sleep(5000)
-    console.log('we are going to publish now')
-    await pub.publish('insert', '8');
-}
-const finish = async (theMessage) => {
-    console.log('fib solution: ',fib(parseInt(theMessage)))
-//     const intFib = fib(parseInt(theMessage))
-//   //  client.connect()
-//      await client.hSet('values', theMessage, intFib,(err, reply) => {
-//     if (err) {
-//         console.error(err);
-//     } else {
-//         console.log('Fib  updated:', reply);}
-//     })
-}
+sub.on('message', (channel, message) => {
+  redisClient.hset('values', message, fib(parseInt(message)));
+});
+sub.subscribe('insert');
 
 function sleep(ms) {
   return new Promise((resolve) => setTimeout(resolve, ms));
@@ -87,6 +39,3 @@ function fib(num) {
         return num2;
     }
 }
-doSubscribe()
-
-//doPublish()
